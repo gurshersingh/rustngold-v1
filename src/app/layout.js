14cce85f0550ed47c0819cdd65b47636f5c6cf61
Gursher Singh <singg3@austin.org.au>
2025-10-06 13:17:40 +1100

    promotion updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -150,10 +150,10 @@ export default function RootLayout({ children }) {
                 <Link 
                     href="https://rust-n-gold.nextorder.com/"
                     className="fixed-button fixed-promo-button">
-                    <span className="promo-click-here">Dine-in Only</span>
-                    <p className="promo-text">KIDS EAT FREE!</p>
-                    <p className="promo-subtext">School Holidays Special</p>
-                    <span className="promo-date">until 5th Oct</span>
+                    <span className="promo-click-here">Online Order Only</span>
+                    <p className="promo-text">10% Discount!</p>
+                    <p className="promo-subtext">Order Online Now</p>
+                    <span className="promo-date">until 31st Oct</span>
                 </Link>
 
                 <Link
